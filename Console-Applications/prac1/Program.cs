a11f2d78c8c834ca09c76136a1aad1859d81b178
Nigel Omondi <nigelomondi62@gmail.com>
2025-05-27 17:05:51 +0300

    Init .net console

---
+++ Console-Applications/prac1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello {name}, today is {currentDate:dddd, MMMM dd, yyyy}.");
