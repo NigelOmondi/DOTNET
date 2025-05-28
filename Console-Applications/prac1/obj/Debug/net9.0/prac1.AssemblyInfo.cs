dbdc34161f802a958b53a154c3bb86ac70419de8
Nigel Omondi <nigelomondi62@gmail.com>
2025-05-28 17:08:25 +0300

    C#

--- Console-Applications/prac1/obj/Debug/net9.0/prac1.AssemblyInfo.cs
+++ Console-Applications/prac1/obj/Debug/net9.0/prac1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prac1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11f2d78c8c834ca09c76136a1aad1859d81b178")]
 [assembly: System.Reflection.AssemblyProductAttribute("prac1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prac1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
